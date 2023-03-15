717ea613bb4bdb68bf629d92b2c989460549042c
PinoMacri <pinonegozio1@outlook.it>
2023-03-15 20:35:57 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from "axios";
+import ProjectCard from "./components/projects/ProjectCard.vue";
+const apiBaseUrl = "http://localhost:8000/api";
+export default {
+  name: "App",
+  components: { ProjectCard },
+  data: () => ({
+    projects: []
+  }),
+  methods: {
+    fetchProjects() {
+      axios.get(apiBaseUrl + "/projects").then(res => {
+        this.projects = res.data;;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+};
+</script>
+
+<template>
+  <main>
+    <ProjectCard :projects="projects" />
+  </main>
+</template>
+
+<style ></style>
---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "ProjectCard",
+    props: { projects: Array }
+};
+</script>
+
+<template>
+    <ul v-if="projects.length >= 1">
+        <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+    </ul>
+    <h4 v-else>Non ci sono progetti</h4>
+</template>
+
+<style></style>
